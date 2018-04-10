6c14d929bfe45a6bc94bc1f2fb733fadd40419b1
jonesga1 <jonesga@s.dcsdk12.org>
2018-04-10 09:39:05 -0600

    Add delete method for BST and finish constructor

--- MovieDatabase/BinarySearchTree.cpp
+++ MovieDatabase/BinarySearchTree.cpp
@@ -2,5 +2,17 @@
 
 template <class T>
 BinarySearchTree<T>::BinarySearchTree() {
+	rootNode = nullptr;
+}
 
+template <class T>
+void BinarySearchTree<T>::deleteTree(BSTNode * nodeToDelete) {
+	if (nodeToDelete == nullptr) { //You've reached the end of a tree
+		return; //Stop what we're doing.
+	}
+	//Traverse and delete both subtrees recursively.
+	deleteTree(nodeToDelete->left); 
+	deleteTree(nodeToDelete->right);
+	//delete the current node
+	delete nodeToDelete;
 }
--- MovieDatabase/BinarySearchTree.h
+++ MovieDatabase/BinarySearchTree.h
@@ -6,15 +6,19 @@
 template <class T> 
 class BinarySearchTree {
 private:
+	//Each node contains a piece of data (some class), as well as pointers to a left and right node.
+	//By default both pointers are null.
 	struct BSTNode {
 		T data;
-		BSTNode * left;
-		BSTNode * right;
+		BSTNode * left = nullptr;
+		BSTNode * right = nullptr;
 	};
 
+	//The very first node
 	BSTNode * rootNode;
 public:
 	BinarySearchTree();
+	void deleteTree(BSTNode * nodeToDelete);
 };
 
 #endif
