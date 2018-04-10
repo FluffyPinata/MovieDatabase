ba6c739085197fffeaed389b66feb4073122d452
jonesga1 <jonesga@s.dcsdk12.org>
2018-04-10 10:12:56 -0600

    Add setters/getters for actor

--- MovieDatabase/Actor.h
+++ MovieDatabase/Actor.h
@@ -12,6 +12,16 @@ private:
 public:
 	Actor();
 	Actor(int _year, std::string _name, std::string _award, std::string _film, bool _winner);
+	int getYear() { return year; }
+	void setYear(int _year) { year = _year; }
+	std::string getName() { return name; }
+	void setName(std::string _name) { name = _name; }
+	std::string getAward() { return award; }
+	void setAward(std::string _award) { award = _award; }
+	std::string getFilm() { return film; }
+	void setFilm(std::string _film) { film = _film; }
+	bool getWinner() { return winner; }
+	void setWinner(bool _winner) { winner = _winner; }
 };
 
 #endif
