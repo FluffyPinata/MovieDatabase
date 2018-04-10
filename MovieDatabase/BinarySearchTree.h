eacfd3fd78e2af2619e394189c37044b176c48a9
jonesga1 <jonesga@s.dcsdk12.org>
2018-04-10 10:08:27 -0600

    Removed .cpp for BST and just included everything in the header

--- MovieDatabase/BinarySearchTree.h
+++ MovieDatabase/BinarySearchTree.h
@@ -21,4 +21,21 @@ public:
 	void deleteTree(BSTNode * nodeToDelete);
 };
 
+template <class T>
+BinarySearchTree<T>::BinarySearchTree() {
+	rootNode = nullptr;
+}
+
+template <class T>
+void BinarySearchTree<T>::deleteTree(BSTNode * nodeToDelete) {
+	if (nodeToDelete == nullptr) { //You've reached the end of a tree
+		return; //Stop what we're doing.
+	}
+	//Traverse and delete both subtrees recursively.
+	deleteTree(nodeToDelete->left);
+	deleteTree(nodeToDelete->right);
+	//delete the current node
+	delete nodeToDelete;
+}
+
 #endif
