8d06c3643a477ce5044505d7834d7e923bc6b426
jonesga1 <jonesga@s.dcsdk12.org>
2018-04-10 07:40:45 -0600

    Create BST

---
+++ MovieDatabase/BinarySearchTree.h
@@ -0,0 +1,12 @@
+#ifndef BINARYSEARCHTREE_H
+#define BINARYSEARCHTREE_H
+
+template <class T>; // Created this as a template so we can use this for all types of data
+class BinarySearchTree {
+private:
+
+public:
+	BinarySearchTree();
+};
+
+#endif
